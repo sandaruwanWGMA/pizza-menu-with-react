af07bf2d12eb22c9889a6c64c25e7f19e6489c71
SandaruwanWGMA <molinduachintha24@gmail.com>
2023-08-08 09:34:38 +0530

    commit

--- src/App/Fact.js
+++ src/App/Fact.js
@@ -7,12 +7,14 @@ function Facts({ number, title, isHidden }) {
 
   return (
     <div
-          onClick={() => {
-              setIsActive(true)
-              setHidden(!hidden)
-              console.log(isActive);
-          }}
-      className={`hiddenBox ${isActive ? "active" : ""} ${!hidden ? "NotHidden" : ""}`}
+      onClick={() => {
+        setIsActive(true);
+        setHidden(!hidden);
+        console.log(isActive);
+      }}
+      className={`hiddenBox ${isActive ? "active" : ""} ${
+        !hidden ? "NotHidden" : ""
+      }`}
     >
       <h1 className="number">{number}</h1>
       <h1 className="title">{title}</h1>
@@ -36,19 +38,16 @@ function Facts({ number, title, isHidden }) {
         />
       </svg>
       <p className="description">
-        Paragraphs are the building blocks of papers. Many students define
-        paragraphs in terms of length: a paragraph is a group of at least five
-        sentences, a paragraph is half a page long, etc. In reality, though, the
-        unity and coherence of ideas among sentences is what constitutes a
-        paragraph.
+        Pizzeria is a charming Italian-inspired pizza restaurant located in the
+        heart of Anytown. With a commitment to delivering authentic flavors and
+        quality ingredients, we offer a diverse menu that caters to all pizza
+        enthusiasts. Our cozy and inviting ambiance provides the perfect setting
+        for family gatherings, date nights, and casual outings.
       </p>
       <ul className="descriptionList">
-        <li>Paragraphs are the building blocks of papers</li>
-        <li>Many students define paragraphs in terms of length</li>
-        <li>
-          Paragraph is a group of at least five sentences, a paragraph is half a
-          page long, etc
-        </li>
+        <li>Restaurant Name: Pizzeria</li>
+        <li>Location: 123 Main Street, Anytown, USA</li>
+        <li>Phone Number: (555) 123-4567</li>
       </ul>
     </div>
   );
